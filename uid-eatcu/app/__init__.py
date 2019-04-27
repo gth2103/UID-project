bf571746e5e411ac06cb22f9a3aefd5761b1b860
Grant <gth2103@columbia.edu>
2019-04-27 13:08:53 -0400

    fixed errors in code, server now running, started registration and login templates

--- uid-eatcu/app/__init__.py
+++ uid-eatcu/app/__init__.py
@@ -1,3 +1,5 @@
+
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -5,6 +7,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
 db = SQLAlchemy(app)
 
